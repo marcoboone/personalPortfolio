150c43d4da86eeefd066ef10008b8af0e70effb4
marcoboone <marcoeboone@outlook.com>
2024-10-05 15:32:57 -0400

    week 5 updates

--- src/app/si/page.jsx
+++ src/app/si/page.jsx
@@ -1,28 +1,33 @@
 import React from 'react'
 import Link from 'next/link'
+import HeroCard from '../components/heroCard'
 
 const SupplementalInstruction = () => {
   return (
-    <div className='w-screen md:h-screen bg-atomwhite'>
-        <div className='h-full w-full p-4 md:p-10 lg:p-20 flex flex-col gap-8 md:gap-10'>
+    <div className='w-screen bg-atomwhite min-h-screen'>
+        <div className='w-full p-4 md:p-10 lg:p-20 flex flex-col gap-8 md:gap-10'>
             <h1 className='text-4xl md:text-6xl text-atomgreyb'>Hello and Welcome to Math 215 SI Sessions!</h1>
             <p className='text-2xl md:text-4xl text-atomgreyb'>My name is Marco and I will be your SI Leader for this course.</p>
             <div className = "flex flex-col gap-4">
             <p className='text-lg text-atomgreyb'>The sessions will be held on: Sunday from 1:00 pm to 2:30 pm</p>
             <p className='text-lg text-atomgreyb'>The sessions will be held at: 1372 East Hall - Central Campus</p>
             </div>
+           
+
             <div className="flex flex-col gap-4">
             <div className="collapse bg-atomwhite text-atomgreyb border-2">
   <input type="radio" name="my-accordion-1" defaultChecked />
   <div className="collapse-title text-xl font-medium">Session Content</div>
   <div className="flex flex-col collapse-content">
-    <Link href = "https://drive.google.com/file/d/1RQVWHWlQvcOgynPZh8C-4tRGvWJiLk-Q/view?usp=drive_link" className = "hover:text-atomblack">Week 1 P-SET</Link>
-    <Link href = "https://drive.google.com/file/d/16BSWou6FRsFD6-O4T_DlhQo-SRADSn_O/view?usp=drive_link" className = "hover:text-atomblack">Week 1 SOLUTIONS</Link>
-    <Link href = "https://drive.google.com/file/d/1ZEfFbsLBlrgOs44XYlZ0nnbU-4v8EMhN/view?usp=drive_link" className = "hover:text-atomblack">Week 2 P-SET</Link>
-    <Link href = "https://drive.google.com/file/d/1L2MwQf5972eToJF5LNJL8FjQbxA8Kfxi/view?usp=sharing" className = "hover:text-atomblack">Week 2 SOLUTIONS</Link>
-    <Link href = "https://drive.google.com/drive/folders/12Pt0F6Cl5k8LezmfKdS41UT9HTURTZ5k?usp=sharing" className = "hover:text-atomblack">Google Drive</Link>
+    <Link className = "hover:text-atomblack" href = "https://drive.google.com/drive/folders/1-IXmYfHPH1a93Pv9lK_R4wvtofhdjgdV?usp=drive_link">Week 1</Link>
+    <Link className = "hover:text-atomblack" href = "https://drive.google.com/drive/folders/1TRzpVUhAfQPte8jxW-AYRA_CtjCD_Woe?usp=drive_link">Week 2</Link>
+    <Link className = "hover:text-atomblack" href = "https://drive.google.com/drive/folders/1xuCvPrqWXHE6t_krGJAoPwDVVAV20NPM?usp=drive_link">Week 3</Link>
+    <Link className = "hover:text-atomblack" href = "/si">Week 4 EXAM NA</Link>
+    <Link className = "hover:text-atomblack" href = "https://drive.google.com/drive/folders/1-FCVV6Z49X1FyQACVflDNmSSaVak-NiT?usp=drive_link">Week 5</Link>
+
   </div>
 </div>
+
 <div className="collapse bg-atomwhite text-atomgreyb border-2">
   <input type="radio" name="my-accordion-1" />
   <div className="collapse-title text-xl font-medium">What are SI Sessions?</div>
