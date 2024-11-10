d46b45051755cecca6bbe7d1cb43d55f7281f1a6
marcoboone <marcoeboone@outlook.com>
2024-11-09 23:00:46 -0500

    week 7

--- src/app/si/page.jsx
+++ src/app/si/page.jsx
@@ -25,6 +25,7 @@ const SupplementalInstruction = () => {
     <Link className = "hover:text-atomblack" href = "https://drive.google.com/drive/folders/1-FCVV6Z49X1FyQACVflDNmSSaVak-NiT?usp=drive_link">SESSION 4</Link>
     <Link className = "hover:text-atomblack" href = "https://drive.google.com/drive/folders/1-I1fVgf8-Z3eevwavWAWsdYQcwBQspFC?usp=drive_link">SESSION 5</Link>
     <Link className = "hover:text-atomblack" href = "https://drive.google.com/drive/folders/1VOQf5YnKe4Y_je3AaXHJTRh_JGYkaE4z?usp=drive_link">SESSION 6</Link>
+    <Link className = "hover:text-atomblack" href = "https://drive.google.com/drive/folders/1-OReSTiPbNrpjSXWO1OBn-DSG0-Nnd5d?usp=drive_link">SESSION 7</Link>
 
   </div>
 </div>
