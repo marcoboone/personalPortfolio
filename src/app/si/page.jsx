63478e52265801f70413a320bd74d4b058de77fa
Marco Boone <marcoeboone@outlook.com>
2025-02-22 16:56:33 -0500

    pset5

--- src/app/si/page.jsx
+++ src/app/si/page.jsx
@@ -75,6 +75,7 @@ const SI = () => {
                     <Link href = "PS2.pdf" className = "">Week 2</Link>
                     <Link href = "PS3.pdf" className = "">Week 3</Link>
                     <Link href = "PS4.pdf" className = "">Week 4</Link>
+                    <Link href = "PS5.pdf" className = "">Week 5</Link>
                    
                 </ul>
                 </div>
